aae0040111ef29d509d48eb6eeb8ba0cd0c85b10
Oliver Cairncross <op.cairncross@gmail.com>
2018-10-07 19:58:16 +1000

    ...

---
+++ dist/server.js
@@ -0,0 +1,179 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const os = require("os");
+const formidable = require("formidable");
+const util = require("util");
+const uuid = require("uuid");
+const mkdirp = require("mkdirp");
+const config = require("config");
+const database_1 = require("./database");
+let meshBase = config.get('meshBase');
+let u = util.inspect;
+let db = new database_1.DBIO();
+let server2 = http.createServer((req, res) => {
+    console.log(`${req.method}: ${req.url}`);
+    if (req.method === "POST") {
+        let form = new formidable.IncomingForm();
+        form.maxFileSize = 1024 * 1024 * 500 * 2;
+        let fileName = uuid.v4();
+        form.on('fileBegin', (name, file) => {
+            let filePath = path.join(config.get("imageBase"), fileName.substr(0, 2), fileName.substr(2, 2));
+            if (!fs.existsSync(filePath)) {
+                mkdirp.sync(filePath);
+            }
+            file.path = path.join(filePath, fileName);
+            console.log(`wrote: ${file.path}`);
+        });
+        form.parse(req, (err, fields, files) => {
+            let frame = {
+                experimentName: fields.experimentName,
+                directory: fields.directory,
+                channelNumber: parseInt(fields.channelNumber),
+                channelName: fields.channelName,
+                timepoint: parseInt(fields.sequenceNumber),
+                width: parseInt(fields.width),
+                height: parseInt(fields.height),
+                depth: parseInt(fields.depth),
+                xSize: parseFloat(fields.xScale),
+                ySize: parseFloat(fields.yScale),
+                zSize: parseFloat(fields.zScale),
+                filename: fileName,
+                msec: parseInt(fields.msec)
+            };
+            let filename = files['byte-buffer'].path;
+            db.insertFrame(frame)
+                .then(value => {
+                let uploadState = 'uploaded';
+                if (value === 'duplicate image') {
+                    uploadState = 'skipped (duplicate)';
+                    deleteFile(filename);
+                }
+                console.log(`${uploadState} image: ${frame.experimentName} channel: ${frame.channelNumber} frame: ${frame.timepoint} file: ${filename}`);
+            })
+                .catch(err => {
+                console.log(`database error inserting image: ${frame.experimentName} channel: ${frame.channelNumber} frame: ${frame.timepoint} file: ${filename}`);
+                deleteFile(filename);
+            });
+            res.end();
+        });
+    }
+    else {
+        res.end();
+    }
+});
+let server = http.createServer((req, res) => {
+    if (!allowAccess(req)) {
+        console.log(`rejected connection`);
+        res.end();
+    }
+    else {
+        if (req.method === "POST") {
+            let form = new formidable.IncomingForm();
+            form.maxFileSize = 1024 * 1024 * 500 * 2;
+            let fileName = uuid.v4();
+            form.on('fileBegin', (name, file) => {
+                let filePath = path.join(config.get("imageBase"), fileName.substr(0, 2), fileName.substr(2, 2));
+                if (!fs.existsSync(filePath)) {
+                    mkdirp.sync(filePath);
+                }
+                file.path = path.join(filePath, fileName);
+            });
+            form.parse(req, (err, fields, files) => {
+                res.writeHead(200, { 'content-type': 'text/plain' });
+                res.write('received upload:\n\n\n');
+                let frame = {
+                    experimentName: fields.experimentName,
+                    directory: fields.directory,
+                    channelNumber: parseInt(fields.channelNumber),
+                    channelName: fields.channelName,
+                    timepoint: parseInt(fields.timepoint),
+                    width: parseInt(fields.width),
+                    height: parseInt(fields.height),
+                    depth: parseInt(fields.depth),
+                    xSize: parseFloat(fields.xScale),
+                    ySize: parseFloat(fields.yScale),
+                    zSize: parseFloat(fields.zScale),
+                    filename: fileName,
+                    msec: parseInt(fields.msec)
+                };
+                let filename = files['byte-buffer'].path;
+                db.insertFrame(frame)
+                    .then(value => {
+                    let uploadState = 'uploaded';
+                    if (value === 'duplicate image') {
+                        uploadState = 'skipped (duplicate)';
+                        deleteFile(filename);
+                    }
+                    console.log(`${uploadState} image: ${frame.experimentName} channel: ${frame.channelNumber} frame: ${frame.timepoint} file: ${filename}`);
+                })
+                    .catch(err => {
+                    console.log(`database error inserting image: ${frame.experimentName} channel: ${frame.channelNumber} frame: ${frame.timepoint} file: ${filename}`);
+                    deleteFile(filename);
+                });
+                res.end();
+            });
+        }
+        else {
+            const fileInfo = getFile(req.url);
+            if (fileInfo) {
+                res.writeHead(200, { "Content-Type": fileInfo.mimeType });
+                fileInfo.fileStream.pipe(res);
+            }
+            else {
+                res.writeHead(404, { "Content-Type": "application/x-binary" });
+                res.end();
+            }
+        }
+    }
+});
+function deleteFile(filename) {
+    fs.unlink(filename, (err) => {
+        if (err) {
+            console.log(`error deleting duplicate image file ${filename}`);
+        }
+    });
+}
+server2.listen(1337);
+console.log("Phoebe server is listening");
+console.log(`Host: ${os.hostname}\nMesh base: ${meshBase}\nImage base: ${config.get("imageBase")}`);
+function getFile(url) {
+    let mimeType;
+    let fileName = url.substr(1);
+    console.log(`looking for ${fileName}`);
+    fileName = path.join(meshBase, fileName.substr(0, 2), fileName.substr(2, 2), url);
+    mimeType = "application/x-binary";
+    if (fs.existsSync(fileName)) {
+        const fileStream = fs.createReadStream(fileName);
+        console.log(`${new Date().toLocaleString()}: ${url} OK`);
+        return { fileStream, mimeType, fileName };
+    }
+    else {
+        console.log(`${new Date().toLocaleString()}: ${url} file not found`);
+        return null;
+    }
+}
+function allowAccess(req) {
+    let remoteAddress = req.connection.remoteAddress;
+    let realIP = req.headers['x-forwarded-for'];
+    //console.log(`headers: ${jss(req,null,3)}`);
+    if (remoteAddress.includes('127.0.0.1')) {
+        return true;
+    }
+    let allowedIP = new Set([
+        '127.0.0.1',
+        '192.168.77.111',
+        '192.168.10.4',
+        '192.168.77.143'
+    ]);
+    if (allowedIP.has(remoteAddress)) {
+        return true;
+    }
+    return true;
+}
+function allowAllAccess() {
+    return true;
+}
+//# sourceMappingURL=server.js.map
